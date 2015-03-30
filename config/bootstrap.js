40b2a2b95cb7a7fa62c827c875db6699e440607b
Michael Gaeta <michael.gaeta@af83.com>
2015-03-30 17:55:46 +0200

    update cron: one request by day

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -9,25 +9,56 @@
  * http://sailsjs.org/#/documentation/reference/sails.config/sails.config.bootstrap.html
  */
 
+var fs = require('fs');
+
 module.exports.bootstrap = function(cb) {
+
+  var checkLastImport = function() {
+    fs.stat(".tmp/lastimport.txt", function(err,stats){
+      if(err) {
+        fs.writeFile(".tmp/lastimport.txt", "", function(err) {
+          if(err) return console.log(err);
+          importation();
+          console.log("The file was created!");
+        });
+      } else {
+        console.log(stats);
+        if ( +stats.mtime.getTime() + (24*60*60*1000) < +(new Date()) ) {
+          importation();
+        }
+      }
+
+
+    });
+  }
+
+  var importation = function() {
+    require('http').get({
+        host: 'localhost',
+        port: 1337,
+        path: '/import'
+    }, function(res){
+      console.log("Finish import");
+      fs.writeFile(".tmp/lastimport.txt", +new Date(), function(err) {
+        if(err) return console.log(err);
+        console.log("The file was saved!");
+      });
+    });
+  }
 
   //cron : récupération des data
   var CronJob = require('cron').CronJob;
   var job = new CronJob({
-    cronTime: '00 00 01 * * 1-5',//du lundi au vendredi à 1h
+    cronTime: '00 16 03 * * 1-5',//du lundi au vendredi à 1h
     onTick: function() {
       console.log("Cron start");
-      return require('http').get({
-          host: 'localhost',
-          port: 1337,
-          path: '/import'
-      }, null);
+      return importation();
     },
     start: false,
     timeZone: 'Europe/Paris'
   });
   job.start();
-
+  checkLastImport();
 
 
   // It's very important to trigger this callback method when you are finished
